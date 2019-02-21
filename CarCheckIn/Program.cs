5faa381d5195999c3fa6b3a9d3ece7396edfb8e1
Akron254 <aaronrono42@gmail.com>
2019-02-21 16:36:26 +0300

    Car Check-In system

---
+++ CarCheckIn/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CarCheckIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckIn Member = new CheckIn();
+           
+            Console.Write("Name: ");
+            Member.Name = Console.ReadLine();
+            Console.Write("Number Plate: ");
+            Member.Numberplate = Console.ReadLine();
+
+            Console.WriteLine("-----------------------------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("NAME: {0} ",Member.Name);
+            Console.WriteLine("NUMBERPLATE: {0} ", Member.Numberplate);
+            Console.WriteLine("TIME: {0} ", Member.Date);
+
+            Console.ReadKey();
+        }
+
+    }
+    class CheckIn
+    {
+        public string Name { get; set; }
+        public string Numberplate { get; set; }
+        public DateTime Date { get; set; }
+
+
+        public CheckIn()
+        {
+            Name = "Unknown";
+            Numberplate = "Unregistered";
+            Date = DateTime.Now;
+        }
+    }
+}
